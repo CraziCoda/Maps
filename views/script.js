c22718a1f0151c7ab11c22bc30266e641539f9a1
Jeff Sarpong <crazicoda@gmail.com>
2021-04-17 11:53:21 +0000

    Update script.js

--- views/script.js
+++ views/script.js
@@ -113,10 +113,10 @@ function getParameterByName(name, url = window.location.href) {
 }
 
 //Device orientation
-let out = document.getElementById('events');
 window.addEventListener('deviceorientation', handleorientation, false);
 
 function handleorientation(event){
+  let out = document.getElementById('events');
   let absolute = event.absolute;
   let alpha    = event.alpha;
   let beta     = event.beta;
