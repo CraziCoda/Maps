6b2da67094d6b13094349b2ea6494443a803fce9
Jeff Sarpong <crazicoda@gmail.com>
2021-04-24 12:08:59 +0000

    mongo

--- database/index.js
+++ database/index.js
@@ -1,1 +1,5 @@
 const mongoose = require('mongoose');
+
+//initialize database
+const mongoUrl = 'mongodb+srv://crazecode:mongoaccess014@firstcluster.4rc4s.mongodb.net/maps?retryWrites=true&w=majority';
+let mongoUrlLocal = 'http://localhost:27017';
